7a25825c32c7afbe45e0628fa14dc52e19ef54d9
Yuriy Glukhov <yuriy.glukhov@gmail.com>
2020-03-03 11:35:36 +0200

    Added pixbuf

--- layers/ui.sh
+++ layers/ui.sh
@@ -3,6 +3,6 @@ ENV DISPLAY :1.0
 
 # SCRIPT
 
-apt_install fonts-dejavu-core xvfb libsdl2-dev libx11-dev libgtk-3-0 iceweasel default-jre
+apt_install fonts-dejavu-core xvfb libsdl2-dev libx11-dev libgtk-3-0 iceweasel default-jre libgdk-pixbuf2.0-dev
 echo 'Xvfb :1 -screen 0 1024x768x16 +extension RANDR &' > /onStart.d/010-start-x-server.sh
 
